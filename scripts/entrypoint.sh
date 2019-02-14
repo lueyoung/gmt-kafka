e4cc3617be4fc0949bbf08bb94cf640230f67138
humstarman <lueyoung7@gmail.com>
2019-02-14 14:37:24 +0800

    update

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,24 @@ set -ex
 info() {
     echo $(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - "$*"
 }
+chk() {
+json=$(curl -s http://restfulsvc.default:8080/api/svc/kafka-np)
+#tmp=$(echo $json | awk -F ':' '{print $1}' | awk -F ":" '{print $2}')
+tmp=$(echo $json | awk -F "\", \"Err" '{print $1}' | awk -F "Ip\": \"" '{print $2}')
+#tmp=$(echo $json | awk -F 'Err' '{print $1}' | awk -F ":" '{print $2}')
+if [[ -z $tmp ]]; then
+  return 0
+fi
+ips=$(echo $tmp | tr "," " ")
+#ips=$(echo $tmp | tr '"' " ")
+n=0
+for ip in $ips; do
+  if [[ -n $ip ]]; then
+    n=$[n+1]
+  fi
+done
+return $n
+}
 
 ZOOKEEPER_PORT=${ZOOKEEPER_PORT:-"2181"}
 ZK_PORT=${ZOOKEEPER_PORT}
@@ -37,7 +55,27 @@ sed -i "s/{{broker.id}}/${ID}/g" /etc/server.properties
 #sed -i "s/\#delete.topic.enable=true/delete.topic.enable=true/g" /opt/kafka/config/server.properties
 sed -i "s/{{zookeeper.nodes}}/${ZK_HOSTS}/g" /etc/server.properties
 #sed -i "s/{{.hostname}}/${THIS_NAME}/g" /etc/server.properties
-sed -i "s/{{.hostname}}/kafka-0,kafka-1,kafka-2/g" /etc/server.properties
+#sed -i "s/{{.hostname}}/kafka-0,kafka-1,kafka-2/g" /etc/server.properties
 echo -e "\n\nlog.cleaner.enable=true\n" >> /etc/server.properties
+N=0
+while [[ $N -lt 3 ]]; do
+  JSON=$(curl -s http://restfulsvc.default:8080/api/svc/kafka-np)
+  #tmp=$(echo $json | awk -F ':' '{print $1}' | awk -F ":" '{print $2}')
+  TMP=$(echo $JSON | awk -F "\", \"Err" '{print $1}' | awk -F "Ip\": \"" '{print $2}')
+  #tmp=$(echo $json | awk -F 'Err' '{print $1}' | awk -F ":" '{print $2}')
+  if [[ -z $TMP ]]; then
+    continue 
+  fi
+  IPS=$(echo $TMP | tr "," " ")
+  #ips=$(echo $tmp | tr '"' " ")
+  N=0
+  for IP in $IPS; do
+    if [[ -n $IP ]]; then
+      N=$[N+1]
+    fi
+  done
+done
+HOSTS=$TMP
+sed -i "s/{{.hostname}}/${HOSTS}/g" /etc/server.properties
 
 kafka-server-start.sh /etc/server.properties
